52db271a563d76f2e30ddcb6c603085dd868e91c
Harry Q <87745547+Harry2166@users.noreply.github.com>
2025-05-05 21:52:56 +0800

    chore: change STOPLIGHT_ID to STOPLIGHT_GROUP_ID

--- lib/Private/_Private.h
+++ lib/Private/_Private.h
@@ -3,7 +3,7 @@
 
 #define WIFI_SSID "wifi-ssid"
 #define WIFI_PASSWORD "wifi-password"
-#define STOPLIGHT_ID "69" // replace with actual stoplight ID
+#define STOPLIGHT_GROUP_ID "69" // replace with actual stoplight group ID
 #define WS_HOST "domain-name.com"
 #define WS_PORT 80 // 80 for ws, 443 for wss
 #define WS_URL "/url-path/"
